a91e184eb82dfb58155b9fa42bad57eeebef5370
A Zaker <gloryfaircompany@protonmail.com>
2020-02-08 15:44:04 +0600

    Add new data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,7 @@ const request = require("request");
 
 
 const forecast = (lat, long, callback) => {
-    const url = "https://api.darksky.net/forecast/" + process.env.WEATHER_KEY + "/" + lat + "," + long;
+    const url = "https://api.darksky.net/forecast/" + process.env.WEATHER_KEY + "/" + lat + "," + long + "?units=si";
     request({ url, json: true}, (error, {body}) => {
         if(error){
             callback("Unable to connect to weather service", undefined);
@@ -23,7 +23,9 @@ const forecast = (lat, long, callback) => {
         }else{
             let temp = body.currently.temperature;
             let prec = body.currently.precipProbability;
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + temp + ' degrees out. There is ' + prec + '% chance of rain!');
+            let tempHigh = body.daily.data[0].temperatureHigh;
+            let tempLow = body.daily.data[0].temperatureLow;
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + temp + ' degrees out. There is ' + prec + '% chance of rain! The current high of the day is ' + tempHigh + ' and the current low is ' + tempLow + '.');
         }
     })
 }
