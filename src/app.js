9307529412b925f8756dff43837e6696cd64f9a7
A Zaker <gloryfaircompany@protonmail.com>
2020-03-24 02:02:49 +0600

    Init commit

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const hbs = require('hbs');
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 
 
